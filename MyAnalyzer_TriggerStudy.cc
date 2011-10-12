77ca83885be598c526eb8a9c2d3942c32b7e65a4
ferencek <>
2011-10-12 19:24:42 +0000

    switched the final event selection to CaloJets

--- MyAnalyzer_TriggerStudy.cc
+++ MyAnalyzer_TriggerStudy.cc
@@ -170,6 +170,8 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    
    edm::Handle<vector<double> > PFJetPt;
    iEvent.getByLabel(edm::InputTag("AK7PFJets:Pt"), PFJetPt);
+   edm::Handle<vector<double> > PFJetPtRaw;
+   iEvent.getByLabel(edm::InputTag("AK7PFJets:PtRaw"), PFJetPtRaw);
    edm::Handle<vector<double> > PFJetEta;
    iEvent.getByLabel(edm::InputTag("AK7PFJets:Eta"), PFJetEta);
    edm::Handle<vector<double> > PFJetPhi;
@@ -179,23 +181,50 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    edm::Handle<vector<int> > PFJetPassLooseID;
    iEvent.getByLabel(edm::InputTag("AK7PFJets:PassLooseID"), PFJetPassLooseID);
 
+   edm::Handle<vector<double> > CaloJetPt;
+   iEvent.getByLabel(edm::InputTag("AK7CaloJets:Pt"), CaloJetPt);
    edm::Handle<vector<double> > CaloJetPtRaw;
    iEvent.getByLabel(edm::InputTag("AK7CaloJets:PtRaw"), CaloJetPtRaw);
    edm::Handle<vector<double> > CaloJetEta;
    iEvent.getByLabel(edm::InputTag("AK7CaloJets:Eta"), CaloJetEta);
+   edm::Handle<vector<double> > CaloJetPhi;
+   iEvent.getByLabel(edm::InputTag("AK7CaloJets:Phi"), CaloJetPhi);
+   edm::Handle<vector<double> > CaloJetE;
+   iEvent.getByLabel(edm::InputTag("AK7CaloJets:Energy"), CaloJetE);
+   edm::Handle<vector<int> > CaloJetPassLooseID;
+   iEvent.getByLabel(edm::InputTag("AK7CaloJets:PassLooseID"), CaloJetPassLooseID);
 
    // check trigger
    int jet370_fired = 0;
-   string jet370_name = "HLT_Jet370_v6";
+   string jet370_name = "HLT_Jet370_v1";
+   if( iEvent.id().run() >= 163269 ) jet370_name = "HLT_Jet370_v2";
+   if( iEvent.id().run() >= 170249 ) jet370_name = "HLT_Jet370_v6";;
    unsigned int index = hltConfig.triggerIndex(jet370_name);
-   if( index < triggerResults->size() ) {
+   if( index < triggerResults->size() )
+   {
      if( triggerResults->accept( index ) ) jet370_fired = 1;
-   } else {
+   }
+   else
+   {
      edm::LogWarning("MyAnalyzer::filter") << "Requested HLT path \"" << jet370_name << "\" does not exist";
    }
 
+   // select PF jets that pass loose JetID
+   vector<int> v_idx_pfjet_looseID;
+   for(unsigned int i=0; i<PFJetPt->size(); i++)
+   {
+       if( PFJetPassLooseID->at(i) ) v_idx_pfjet_looseID.push_back(i);
+   }
+
+   // select jets that pass loose JetID
+   vector<int> v_idx_jet_looseID;
+   for(unsigned int i=0; i<CaloJetPt->size(); i++)
+   {
+       if( CaloJetPassLooseID->at(i) ) v_idx_jet_looseID.push_back(i);
+   }
+
    // create a vector of CaloJet indices (for CaloJets with |eta|<getPreCutValue1("jetFidRegion")) ordered by uncorrected jet pT
-   vector<int> v_idx_calojet_uncorr;
+   vector<int> v_idx_jet_uncorr;
 
    vector<pair<size_t, myiter> > order;
    size_t n = 0;
@@ -208,50 +237,53 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
    for(unsigned int i=0; i<order.size(); i++)
    {
-     v_idx_calojet_uncorr.push_back(order[i].first); // all CaloJets with |eta|<getPreCutValue1("jetFidRegion") ordered by ucorrected jet pT
+     v_idx_jet_uncorr.push_back(order[i].first); // all CaloJets with |eta|<getPreCutValue1("jetFidRegion") ordered by ucorrected jet pT
    }
+   
    // check if there are any jets present within |DeltaEta|<getPreCutValue1("jetTriggerDeltaEtaCut") from the leading uncorrected jet
    int deltaEtaJetFound = 0;
-   if(v_idx_calojet_uncorr.size() >= 2)
+   if(v_idx_jet_uncorr.size() >= 2)
    {
-     for(unsigned int i=1; i<v_idx_calojet_uncorr.size(); i++)
+     for(unsigned int i=1; i<v_idx_jet_uncorr.size(); i++)
      {
-       if( CaloJetPtRaw->at(v_idx_calojet_uncorr[i]) < getPreCutValue1("jetTriggerPtThresh") || CaloJetEta->at(v_idx_calojet_uncorr[i]) > getPreCutValue1("jetFidRegion") ) continue;
-       if( fabs(CaloJetEta->at(v_idx_calojet_uncorr[0]) - CaloJetEta->at(v_idx_calojet_uncorr[i])) < getPreCutValue1("jetTriggerDeltaEtaCut"))
+       if( CaloJetPt->at(v_idx_jet_uncorr[i]) < getPreCutValue1("jetTriggerPtThresh") ) continue;
+       if( fabs(CaloJetEta->at(v_idx_jet_uncorr[0]) - CaloJetEta->at(v_idx_jet_uncorr[i])) < getPreCutValue1("jetTriggerDeltaEtaCut"))
        {
          deltaEtaJetFound = 1;
          break;
        }
      }
    }
-      
-   // select jets that pass loose JetID
-   vector<int> v_idx_jet_looseID;
-   for(unsigned int i=0; i<PFJetPt->size(); i++) {
-       // select jets inside a fiducial region
-       if( PFJetPassLooseID->at(i) ) v_idx_jet_looseID.push_back(i);
+
+   int passEEJetFilter = 1;
+   if( v_idx_jet_looseID.size() > 0 )
+   {
+     if( CaloJetPt->at(v_idx_jet_looseID[0]) > 15000 ) passEEJetFilter = 0;
    }
 
    // Set the evaluation of the cuts to false and clear the variable values and filled status
    resetCuts();
 
-   fillVariableWithValue("HLT_Jet370_v6", jet370_fired );
+   fillVariableWithValue("HLT_Jet370", jet370_fired );
+   fillVariableWithValue("PassEEJetFilter", passEEJetFilter );
    fillVariableWithValue("TriggerDeltaEtaCut", deltaEtaJetFound );
 
-   fillVariableWithValue("nJet_all", PFJetPt->size());
+   fillVariableWithValue("nJet_all", CaloJetPt->size());
    fillVariableWithValue("nJet_looseID", v_idx_jet_looseID.size());
    
-   if( v_idx_jet_looseID.size() >= 1 ) {
-       fillVariableWithValue( "absEtaJet1", fabs( PFJetEta->at(v_idx_jet_looseID[0]) ) );
+   if( v_idx_jet_looseID.size() >= 1 )
+   {
+       fillVariableWithValue( "absEtaJet1", fabs( CaloJetEta->at(v_idx_jet_looseID[0]) ) );
    }
-   if( v_idx_jet_looseID.size() >= 2 ) {
-       fillVariableWithValue( "absEtaJet2", fabs( PFJetEta->at(v_idx_jet_looseID[1]) ) );
+   if( v_idx_jet_looseID.size() >= 2 )
+   {
+       fillVariableWithValue( "absEtaJet2", fabs( CaloJetEta->at(v_idx_jet_looseID[1]) ) );
        
        TLorentzVector v_j1j2, v_j1, v_j2;
-       v_j1.SetPtEtaPhiE(PFJetPt->at(v_idx_jet_looseID[0]),PFJetEta->at(v_idx_jet_looseID[0]),PFJetPhi->at(v_idx_jet_looseID[0]),PFJetE->at(v_idx_jet_looseID[0]));
-       v_j2.SetPtEtaPhiE(PFJetPt->at(v_idx_jet_looseID[1]),PFJetEta->at(v_idx_jet_looseID[1]),PFJetPhi->at(v_idx_jet_looseID[1]),PFJetE->at(v_idx_jet_looseID[1]));
+       v_j1.SetPtEtaPhiE(CaloJetPt->at(v_idx_jet_looseID[0]),CaloJetEta->at(v_idx_jet_looseID[0]),CaloJetPhi->at(v_idx_jet_looseID[0]),CaloJetE->at(v_idx_jet_looseID[0]));
+       v_j2.SetPtEtaPhiE(CaloJetPt->at(v_idx_jet_looseID[1]),CaloJetEta->at(v_idx_jet_looseID[1]),CaloJetPhi->at(v_idx_jet_looseID[1]),CaloJetE->at(v_idx_jet_looseID[1]));
        // calculate |DeltaEta(j1,j2)|
-       fillVariableWithValue( "absDeltaEtaJ1J2", fabs( PFJetEta->at(v_idx_jet_looseID[0]) - PFJetEta->at(v_idx_jet_looseID[1]) ) );
+       fillVariableWithValue( "absDeltaEtaJ1J2", fabs( CaloJetEta->at(v_idx_jet_looseID[0]) - CaloJetEta->at(v_idx_jet_looseID[1]) ) );
        // calculate M_j1j2
        v_j1j2 = v_j1 + v_j2;
        
@@ -275,6 +307,114 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    // select only those events that pass the full selection
    ret = passedCut("all");
 
+   // ##################### Event Printout #####################
+
+   // PassJ1J2Mass1000FailTrigger event printout
+   string category = "PassJ1J2Mass1000FailTrigger";
+   if( passedAllPreviousCuts("J1J2Mass1000") && passedCut("J1J2Mass1000") && deltaEtaJetFound==0 )
+   {
+     cout << category << ": ----------- START ------------" << endl;
+     cout << category << ": Run, lumi, event: "<< iEvent.id().run() << ", "
+                                               << iEvent.luminosityBlock() << ", "
+                                               << iEvent.id().event() << endl;
+     // loop over ak7 CaloJets
+     for (unsigned int i=0; i<CaloJetPt->size(); ++i)
+     {
+       cout << category << ": CaloJet " << i << " Pt, PtRaw, eta, phi: " << CaloJetPt->at(i) << ", "
+                                                                         << CaloJetPtRaw->at(i) << ", "
+                                                                         << CaloJetEta->at(i) << ", "
+                                                                         << CaloJetPhi->at(i) << endl;
+     }
+     // loop over ucorrected ak7 CaloJets with |eta|<2.7
+     for (unsigned int i=0; i<v_idx_jet_uncorr.size(); ++i)
+     {
+       cout << category << ": Uncorrected CaloJet " << i << " Pt, PtRaw, eta, phi: " << CaloJetPt->at(v_idx_jet_uncorr[i]) << ", "
+                                                                                     << CaloJetPtRaw->at(v_idx_jet_uncorr[i]) << ", "
+                                                                                     << CaloJetEta->at(v_idx_jet_uncorr[i]) << ", "
+                                                                                     << CaloJetPhi->at(v_idx_jet_uncorr[i]) << endl;
+     }
+     // loop over ak7 CaloJets that pass loose jet ID requirements
+     for (unsigned int i=0; i<v_idx_jet_looseID.size(); ++i)
+     {
+       cout << category << ": PassLooseID CaloJet "<< i << " Pt, PtRaw, eta, phi: " << CaloJetPt->at(v_idx_jet_looseID[i]) << ", "
+                                                                                    << CaloJetPtRaw->at(v_idx_jet_looseID[i]) << ", "
+                                                                                    << CaloJetEta->at(v_idx_jet_looseID[i]) << ", "
+                                                                                    << CaloJetPhi->at(v_idx_jet_looseID[i]) << endl;
+     }
+     // loop over ak7 PFJets
+     for (unsigned int i=0; i<PFJetPt->size(); ++i)
+     {
+       cout << category << ": PFJet " << i << " Pt, PtRaw, eta, phi: " << PFJetPt->at(i) << ", "
+                                                                       << PFJetPtRaw->at(i) << ", "
+                                                                       << PFJetEta->at(i) << ", "
+                                                                       << PFJetPhi->at(i) << endl;
+     }
+     // loop over ak7 PFJets that pass loose jet ID requirements
+     for (unsigned int i=0; i<v_idx_pfjet_looseID.size(); ++i)
+     {
+       cout << category << ": PassLooseID PFJet "<< i << " Pt, PtRaw, eta, phi: " << PFJetPt->at(v_idx_jet_looseID[i]) << ", "
+                                                                                  << PFJetPtRaw->at(v_idx_jet_looseID[i]) << ", "
+                                                                                  << PFJetEta->at(v_idx_jet_looseID[i]) << ", "
+                                                                                  << PFJetPhi->at(v_idx_jet_looseID[i]) << endl;
+     }
+     cout << category << ": |DeltaEtaJ1J2|: "<< getVariableValue("absDeltaEtaJ1J2") << endl;
+     cout << category << ": J1J2Mass: "<< getVariableValue("J1J2Mass") << endl;
+     cout << category << ": ------------ END -------------" << endl;
+   }
+   
+   // PassJ1J2Mass1500FailTrigger event printout
+   category = "PassJ1J2Mass1500FailTrigger";
+   if( passedAllPreviousCuts("J1J2Mass1500") && passedCut("J1J2Mass1500") && deltaEtaJetFound==0 )
+   {
+     cout << category << ": ----------- START ------------" << endl;
+     cout << category << ": Run, lumi, event: "<< iEvent.id().run() << ", "
+                                               << iEvent.luminosityBlock() << ", "
+                                               << iEvent.id().event() << endl;
+     // loop over ak7 CaloJets
+     for (unsigned int i=0; i<CaloJetPt->size(); ++i)
+     {
+       cout << category << ": CaloJet " << i << " Pt, PtRaw, eta, phi: " << CaloJetPt->at(i) << ", "
+                                                                         << CaloJetPtRaw->at(i) << ", "
+                                                                         << CaloJetEta->at(i) << ", "
+                                                                         << CaloJetPhi->at(i) << endl;
+     }
+     // loop over ucorrected ak7 CaloJets with |eta|<2.7
+     for (unsigned int i=0; i<v_idx_jet_uncorr.size(); ++i)
+     {
+       cout << category << ": Uncorrected CaloJet " << i << " Pt, PtRaw, eta, phi: " << CaloJetPt->at(v_idx_jet_uncorr[i]) << ", "
+                                                                                     << CaloJetPtRaw->at(v_idx_jet_uncorr[i]) << ", "
+                                                                                     << CaloJetEta->at(v_idx_jet_uncorr[i]) << ", "
+                                                                                     << CaloJetPhi->at(v_idx_jet_uncorr[i]) << endl;
+     }
+     // loop over ak7 CaloJets that pass loose jet ID requirements
+     for (unsigned int i=0; i<v_idx_jet_looseID.size(); ++i)
+     {
+       cout << category << ": PassLooseID CaloJet "<< i << " Pt, PtRaw, eta, phi: " << CaloJetPt->at(v_idx_jet_looseID[i]) << ", "
+                                                                                    << CaloJetPtRaw->at(v_idx_jet_looseID[i]) << ", "
+                                                                                    << CaloJetEta->at(v_idx_jet_looseID[i]) << ", "
+                                                                                    << CaloJetPhi->at(v_idx_jet_looseID[i]) << endl;
+     }
+     // loop over ak7 PFJets
+     for (unsigned int i=0; i<PFJetPt->size(); ++i)
+     {
+       cout << category << ": PFJet " << i << " Pt, PtRaw, eta, phi: " << PFJetPt->at(i) << ", "
+                                                                       << PFJetPtRaw->at(i) << ", "
+                                                                       << PFJetEta->at(i) << ", "
+                                                                       << PFJetPhi->at(i) << endl;
+     }
+     // loop over ak7 PFJets that pass loose jet ID requirements
+     for (unsigned int i=0; i<v_idx_pfjet_looseID.size(); ++i)
+     {
+       cout << category << ": PassLooseID PFJet "<< i << " Pt, PtRaw, eta, phi: " << PFJetPt->at(v_idx_pfjet_looseID[i]) << ", "
+                                                                                  << PFJetPtRaw->at(v_idx_pfjet_looseID[i]) << ", "
+                                                                                  << PFJetEta->at(v_idx_pfjet_looseID[i]) << ", "
+                                                                                  << PFJetPhi->at(v_idx_pfjet_looseID[i]) << endl;
+     }
+     cout << category << ": |DeltaEtaJ1J2|: "<< getVariableValue("absDeltaEtaJ1J2") << endl;
+     cout << category << ": J1J2Mass: "<< getVariableValue("J1J2Mass") << endl;
+     cout << category << ": ------------ END -------------" << endl;
+   }
+
    //############################# User's code ends here #################################
    //#####################################################################################
    
@@ -289,12 +429,16 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 bool
 MyAnalyzer::endLuminosityBlock(edm::LuminosityBlock& iLumi, edm::EventSetup const& iSetup)
 {
-  if ( skimWasMade_ ) {
+  if ( skimWasMade_ )
+  {
       edm::Handle<edm::MergeableCounter> eventCounter;
 
-      if (iLumi.getByLabel(eventCounterInputTag_, eventCounter) && eventCounter.isValid()) {
+      if (iLumi.getByLabel(eventCounterInputTag_, eventCounter) && eventCounter.isValid())
+      {
           NEvtTotBeforeWeight_ += (double) eventCounter->value;
-      } else {
+      }
+      else
+      {
           edm::LogError("MyAnalyzer::endLuminosityBlock") << "Can't get the product " << eventCounterInputTag_ <<". Please make sure the skimWasMade and eventCounterInputTag parameters were set correctly.";
           exit(1);
       }
