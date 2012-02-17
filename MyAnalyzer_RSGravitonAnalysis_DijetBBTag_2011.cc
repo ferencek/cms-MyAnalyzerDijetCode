04e00022110392d4ad076ba42b29077c20face6a
ferencek <>
2012-02-17 04:18:20 +0000

    updated RSG analysis code

--- MyAnalyzer_RSGravitonAnalysis_DijetBBTag_2011.cc
+++ MyAnalyzer_RSGravitonAnalysis_DijetBBTag_2011.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Dinko Ferencek
 //         Created:  Mon Sep 12 15:06:41 CDT 2011
-// $Id: MyAnalyzer_bTaggingEfficiency_DijetBBTag_2011.cc,v 1.3 2012/01/31 20:59:17 ferencek Exp $
+// $Id: MyAnalyzer_RSGravitonAnalysis_DijetBBTag_2011.cc,v 1.1 2012/02/08 01:32:19 ferencek Exp $
 //
 //
 
@@ -33,6 +33,8 @@
 #include "FWCore/Utilities/interface/InputTag.h"
 
 #include "DataFormats/Common/interface/MergeableCounter.h"
+
+#include "PhysicsTools/Utilities/interface/LumiReWeighting.h"
 
 // BaseClass
 #include "MyAnalysis/MyAnalyzer/interface/BaseClass.h"
@@ -69,7 +71,7 @@ class MyAnalyzer : public BaseClass, public edm::EDFilter {
       virtual bool endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
 
       // ----------member data ---------------------------
-
+      edm::LumiReWeighting LumiWeights;
 };
 
 //
@@ -109,9 +111,29 @@ MyAnalyzer::beginJob()
    //########################### User's code starts here #################################/
    
    // book your histograms here
-   
+   CreateUserTH1D("h1_DijetMass_bbbar_0tag", getHistoNBins("DijetMass"), getHistoMin("DijetMass"), getHistoMax("DijetMass"));
+   CreateUserTH1D("h1_DijetMass_bbbar_1tag", getHistoNBins("DijetMass"), getHistoMin("DijetMass"), getHistoMax("DijetMass"));
+   CreateUserTH1D("h1_DijetMass_bbbar_2tag", getHistoNBins("DijetMass"), getHistoMin("DijetMass"), getHistoMax("DijetMass"));
    
    // initialize your variables here
+
+   // Summer11 PU_S4 distribution obtained by only looking at the in-time crossing.  This is the "spike+smear" distribution, RECOMMENDED FOR REWEIGHTING.
+   double PileUpDistMC_ObservedBX0_d[35] = {1.45346E-01,6.42802E-02,6.95255E-02,6.96747E-02,6.92955E-02,6.84997E-02,6.69528E-02,6.45515E-02,6.09865E-02,5.63323E-02,5.07322E-02,4.44681E-02,3.79205E-02,3.15131E-02,2.54220E-02,2.00184E-02,1.53776E-02,1.15387E-02,8.47608E-03,6.08715E-03,4.28255E-03,2.97185E-03,2.01918E-03,1.34490E-03,8.81587E-04,5.69954E-04,3.61493E-04,2.28692E-04,1.40791E-04,8.44606E-05,5.10204E-05,3.07802E-05,1.81401E-05,1.00201E-05,5.80004E-06};
+   vector<float> PileUpDistMC_ObservedBX0(PileUpDistMC_ObservedBX0_d, PileUpDistMC_ObservedBX0_d + sizeof(PileUpDistMC_ObservedBX0_d) / sizeof(double) );
+   // Run2011A+Run2011B pile-up distribution
+   double PileUpDistData_Observed_d[35] = {13446512.0, 59065300.0, 140902672.0, 241301168.0, 333744896.0, 398710976.0, 430106432.0, 432283008.0, 413820192.0, 382845984.0, 345163680.0, 304343808.0, 262555024.0, 221330752.0, 181982560.0, 145689760.0, 113413440.0, 85778864.0, 63012392.0, 44959588.0, 31169040.0, 21007856.0, 13775880.0, 8796407.0, 5474417.5, 3323775.5, 1970637.75, 1142040.5, 647538.625, 359547.1875, 195673.15625, 104459.9453125, 54745.15234375, 28185.56640625, 28005.548828125};
+   vector<float> PileUpDistData_Observed(PileUpDistData_Observed_d, PileUpDistData_Observed_d + sizeof(PileUpDistData_Observed_d) / sizeof(double) );
+   // Run2011A pile-up distribution
+   double PileUpDistDataRun2011A_Observed_d[35] = {12965370.0, 55851368.0, 129329360.0, 212133600.0, 276137728.0, 303603552.0, 293257504.0, 255632864.0, 204970016.0, 153263664.0, 107935616.0, 72100608.0, 45912988.0, 27970044.0, 16342576.0, 9175983.0, 4958610.0, 2582392.75, 1297695.75, 629975.0625, 295784.25, 134469.671875, 59260.0703125, 25343.8671875, 10530.08984375, 4255.04833984375, 1673.949462890625, 641.7764892578125, 240.02249145507812, 87.650428771972656, 31.280984878540039, 10.919528007507324, 3.7314565181732178, 1.2492282390594482, 0.60236752033233643};
+   vector<float> PileUpDistDataRun2011A_Observed(PileUpDistDataRun2011A_Observed_d, PileUpDistDataRun2011A_Observed_d + sizeof(PileUpDistDataRun2011A_Observed_d) / sizeof(double) );
+   // Run2011B pile-up distribution
+   double PileUpDistDataRun2011B_Observed_d[35] = {481141.72472439631, 3213932.6435495433, 11573306.879849812, 29167566.222834267, 57607165.266923025, 95107416.620279759, 136848927.47142315, 176650126.03691837, 208850159.2230134, 229582323.64430469, 237228075.21969861, 232243229.29014349, 216642041.8648839, 193360704.19149143, 165639997.39323151, 136513771.36073488, 108454824.41062045, 83196475.076050699, 61714695.179401994, 44329612.988056183, 30873256.083125703, 20873387.048466656, 13716620.52313938, 8771062.8496078029, 5463887.6202946259, 3319520.4784049694, 1968963.8927261904, 1141398.7709201651, 647298.63969274936, 359459.56447747251, 195641.88301312848, 104449.02504469089, 54741.419995713957, 28184.317687585703, 28004.947183609398};
+   vector<float> PileUpDistDataRun2011B_Observed(PileUpDistDataRun2011B_Observed_d, PileUpDistDataRun2011B_Observed_d + sizeof(PileUpDistDataRun2011B_Observed_d) / sizeof(double) );
+
+   int puReweightingEra = int(getPreCutValue1("puReweightingEra"));
+   if( puReweightingEra==1 ) LumiWeights = edm::LumiReWeighting(PileUpDistMC_ObservedBX0, PileUpDistDataRun2011A_Observed);
+   else if( puReweightingEra==2 ) LumiWeights = edm::LumiReWeighting(PileUpDistMC_ObservedBX0, PileUpDistDataRun2011B_Observed);
+   else LumiWeights = edm::LumiReWeighting(PileUpDistMC_ObservedBX0, PileUpDistData_Observed);
    
    //############################# User's code ends here #################################
    //#####################################################################################
@@ -142,9 +164,11 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    //#####################################################################################
    //########################### User's code starts here #################################
 
-//    int btagger = int(getPreCutValue1("btagger"));
-//    int matchingType = int(getPreCutValue1("matchingType"));
-//    double matchingRadius = getPreCutValue1("matchingRadius");
+   int doPUReweighting = int(getPreCutValue1("doPUReweighting"));
+   double resonanceMass = getPreCutValue1("resonanceMass");
+   int btagger = int(getPreCutValue1("btagger"));
+   int matchingType = int(getPreCutValue1("matchingType"));
+   double matchingRadius = getPreCutValue1("matchingRadius");
    
    // grab necessary objects from the event
    edm::Handle<vector<double> > GenParticlePt;
@@ -161,6 +185,22 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    iEvent.getByLabel(edm::InputTag("GenParticles:Status"), GenParticleStatus);
    edm::Handle<vector<int> > GenParticleMotherIndex;
    iEvent.getByLabel(edm::InputTag("GenParticles:MotherIndex"), GenParticleMotherIndex);
+
+   edm::Handle<vector<unsigned int> > NPU;
+   iEvent.getByLabel(edm::InputTag("GenEventInfo:PileUpNumberOfInteractions"), NPU);
+   edm::Handle<vector<int> > BX;
+   iEvent.getByLabel(edm::InputTag("GenEventInfo:PileUpBunchCrossing"), BX);
+
+   edm::Handle<vector<bool> > PVIsFake;
+   iEvent.getByLabel(edm::InputTag("Vertices:IsFake"), PVIsFake);
+   edm::Handle<vector<double> > PVX;
+   iEvent.getByLabel(edm::InputTag("Vertices:X"), PVX);
+   edm::Handle<vector<double> > PVY;
+   iEvent.getByLabel(edm::InputTag("Vertices:Y"), PVY);
+   edm::Handle<vector<double> > PVZ;
+   iEvent.getByLabel(edm::InputTag("Vertices:Z"), PVZ);
+   edm::Handle<vector<double> > PVNDF;
+   iEvent.getByLabel(edm::InputTag("Vertices:NDF"), PVNDF);
    
    edm::Handle<vector<double> > PFJetPt_;
    iEvent.getByLabel(edm::InputTag("AK7PFJets:Pt"), PFJetPt_);
@@ -187,6 +227,20 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    edm::Handle<vector<int> > PFJetPartonFlavor;
    iEvent.getByLabel(edm::InputTag("AK7PFJets:PartonFlavor"), PFJetPartonFlavor);
 
+   // apply pile-up reweighting
+   if( !iEvent.isRealData() && doPUReweighting )
+   {
+     int npu = -1;
+     for( vector<int>::const_iterator bxIt = BX->begin(); bxIt != BX->end(); ++bxIt )
+     {
+        if( *bxIt == 0 ) {
+          npu = NPU->at( distance(BX->begin(),bxIt) );
+          break;
+        }
+     }
+     // set the event weight
+     eventWeight = LumiWeights.weight( npu );
+   }
    
    auto_ptr<std::vector<double> >  PFJetPt ( new std::vector<double>() );
    auto_ptr<std::vector<double> >  PFJetE  ( new std::vector<double>() );
@@ -208,6 +262,82 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
        v_idx_pfjet_JetID.push_back(i);
    }
 
+   // loop over primary vertices and select good ones
+   vector<int> v_idx_goodPV;
+   for(size_t i=0; i<PVX->size(); i++)
+   {
+     double rho = sqrt(PVX->at(i)*PVX->at(i) + PVY->at(i)*PVY->at(i));
+
+     if( PVIsFake->at(i) ) continue;
+     if( !(PVNDF->at(i) > 4) ) continue;
+     if( !(fabs(PVZ->at(i)) <= 24) ) continue;
+     if( !(rho <= 2) ) continue;
+     v_idx_goodPV.push_back(i);
+   }
+   
+   int nBTaggedJets = 0;
+   int nHeavyFlavorJets = 0;
+   int nBTaggedHeavyFlavorJets = 0;
+
+   if( v_idx_pfjet_JetID.size() >= 2 )
+   {
+     // jet and GenParticle 4-vectors
+     TLorentzVector v_j, v_gp;
+
+     // loop over two leading jets
+     for(size_t i=0; i<2; ++i)
+     {
+       bool isHeavyFlavor = false;
+
+       // set jet 4-vector
+       v_j.SetPtEtaPhiE(PFJetPt->at(v_idx_pfjet_JetID[i]),PFJetEta->at(v_idx_pfjet_JetID[i]),PFJetPhi->at(v_idx_pfjet_JetID[i]),PFJetE->at(v_idx_pfjet_JetID[i]));
+
+       if( !iEvent.isRealData() )
+       {
+         if( matchingType==0 && abs(PFJetPartonFlavor->at(v_idx_pfjet_JetID[i]))==5 )
+         {
+           ++nHeavyFlavorJets;
+           isHeavyFlavor = true;
+         }
+         else if( matchingType!=0 )
+         {
+           double minDeltaR = 999.;
+
+           // loop over GenParticles
+           for(size_t j=0; j<GenParticlePt->size(); ++j)
+           {
+             int pdgID = abs(GenParticlePdgId->at(j));
+
+             if( pdgID==511 || pdgID==521 || pdgID==531 || pdgID==541 || pdgID==5122 || pdgID==5132 || pdgID==5232 || pdgID==5332
+                 || pdgID==411 || pdgID==421 || pdgID==431 || pdgID==4122 || pdgID==4132 || pdgID==4232 || pdgID==4332 )
+             {
+               // set GenParticle 4-vector
+               v_gp.SetPtEtaPhiE(GenParticlePt->at(j),GenParticleEta->at(j),GenParticlePhi->at(j),GenParticleE->at(j));
+               double deltaR = v_j.DeltaR(v_gp);
+
+               if( deltaR < minDeltaR ) minDeltaR = deltaR;
+             }
+           }
+
+           if( minDeltaR < matchingRadius )
+           {
+             ++nHeavyFlavorJets;
+             isHeavyFlavor = true;
+           }
+         }
+       }
+
+       if( (btagger==0 && PFJetTCHE->at(v_idx_pfjet_JetID[i])>getPreCutValue1("TCHEL_WP")) ||
+           (btagger==1 && PFJetTCHE->at(v_idx_pfjet_JetID[i])>getPreCutValue1("TCHEM_WP")) ||
+           (btagger==2 && PFJetSSVHE->at(v_idx_pfjet_JetID[i])>getPreCutValue1("SSVHEM_WP")) ||
+           (btagger==3 && PFJetTCHP->at(v_idx_pfjet_JetID[i])>getPreCutValue1("TCHPT_WP")) ||
+           (btagger==4 && PFJetSSVHP->at(v_idx_pfjet_JetID[i])>getPreCutValue1("SSVHPT_WP")) )
+       {
+         ++nBTaggedJets;
+         if( isHeavyFlavor ) ++nBTaggedHeavyFlavorJets;
+       }
+     }
+   }
    
    // Set the evaluation of the cuts to false and clear the variable values and filled status
    resetCuts();
@@ -226,100 +356,46 @@ MyAnalyzer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
      }
    }
 
-   fillVariableWithValue( "nSt3_q_fromRSG", nSt3_q_fromRSG );
-   fillVariableWithValue( "nSt3_b_fromRSG", nSt3_b_fromRSG );
+   fillVariableWithValue( "nSt3_q_fromRSG", nSt3_q_fromRSG, eventWeight );
+   fillVariableWithValue( "nSt3_b_fromRSG", nSt3_b_fromRSG, eventWeight );
 
+   fillVariableWithValue("nGoodVertices", v_idx_goodPV.size(), eventWeight );
    
-   fillVariableWithValue("nJets_all", PFJetPt->size());
-   fillVariableWithValue("nJets_JetID", v_idx_pfjet_JetID.size());
+   fillVariableWithValue("nJets_all", PFJetPt->size(), eventWeight );
+   fillVariableWithValue("nJets_JetID", v_idx_pfjet_JetID.size(), eventWeight );
 
    if( v_idx_pfjet_JetID.size() >= 1 )
    {
-     fillVariableWithValue( "absEtaJ1", fabs( PFJetEta->at(v_idx_pfjet_JetID[0]) ));
+     fillVariableWithValue( "absEtaJ1", fabs( PFJetEta->at(v_idx_pfjet_JetID[0]) ), eventWeight );
    }
    if( v_idx_pfjet_JetID.size() >= 2 )
    {
-     fillVariableWithValue( "absEtaJ2", fabs( PFJetEta->at(v_idx_pfjet_JetID[1]) ) );
+     fillVariableWithValue( "absEtaJ2", fabs( PFJetEta->at(v_idx_pfjet_JetID[1]) ), eventWeight );
      
      TLorentzVector v_j1j2, v_j1, v_j2;
      v_j1.SetPtEtaPhiE(PFJetPt->at(v_idx_pfjet_JetID[0]),PFJetEta->at(v_idx_pfjet_JetID[0]),PFJetPhi->at(v_idx_pfjet_JetID[0]),PFJetE->at(v_idx_pfjet_JetID[0]));
      v_j2.SetPtEtaPhiE(PFJetPt->at(v_idx_pfjet_JetID[1]),PFJetEta->at(v_idx_pfjet_JetID[1]),PFJetPhi->at(v_idx_pfjet_JetID[1]),PFJetE->at(v_idx_pfjet_JetID[1]));
      // calculate |DeltaEta(j1,j2)|
-     fillVariableWithValue( "absDeltaEtaJ1J2", fabs( PFJetEta->at(v_idx_pfjet_JetID[0]) - PFJetEta->at(v_idx_pfjet_JetID[1]) ) );
+     fillVariableWithValue( "absDeltaEtaJ1J2", fabs( PFJetEta->at(v_idx_pfjet_JetID[0]) - PFJetEta->at(v_idx_pfjet_JetID[1]) ), eventWeight );
      // calculate M_j1j2
      v_j1j2 = v_j1 + v_j2;
-     fillVariableWithValue( "DijetMass", v_j1j2.M() );
+     fillVariableWithValue( "DijetMass", v_j1j2.M(), eventWeight );
 
-     fillVariableWithValue( "DijetMass_qqbar", getVariableValue("DijetMass") );
-     fillVariableWithValue( "DijetMass_bbbar", getVariableValue("DijetMass") );
+     fillVariableWithValue( "DijetMass_qqbar", getVariableValue("DijetMass"), eventWeight );
+     fillVariableWithValue( "DijetMass_bbbar", getVariableValue("DijetMass"), eventWeight );
+     fillVariableWithValue( "x_bbbar", (getVariableValue("DijetMass")/resonanceMass), eventWeight );
    }
    
    // Evaluate cuts (but do not apply them)
    evaluateCuts();
-   
 
-//    int nBTaggedJets = 0;
-//    int nHeavyFlavorJets = 0;
-//    int nBTaggedHeavyFlavorJets = 0;
-//    
-//    if( v_idx_pfjet_JetID.size() >= 2 )
-//    {
-//      // jet and GenParticle 4-vectors
-//      TLorentzVector v_j, v_gp;
-// 
-//      // loop over two leading jets
-//      for(size_t i=0; i<2; ++i)
-//      {
-//        bool isHeavyFlavor = false;
-// 
-//        // set jet 4-vector
-//        v_j.SetPtEtaPhiE(PFJetPt->at(v_idx_pfjet_JetID[i]),PFJetEta->at(v_idx_pfjet_JetID[i]),PFJetPhi->at(v_idx_pfjet_JetID[i]),PFJetE->at(v_idx_pfjet_JetID[i]));
-// 
-//        if( !iEvent.isRealData() )
-//        {
-//          if( matchingType==0 && abs(PFJetPartonFlavor->at(v_idx_pfjet_JetID[i]))==5 )
-//          {
-//            ++nHeavyFlavorJets;
-//            isHeavyFlavor = true;
-//          }
-//          else if( matchingType!=0 )
-//          {
-//            double minDeltaR = 999.;
-// 
-//            // loop over GenParticles
-//            for(size_t j=0; j<GenParticlePt->size(); ++j)
-//            {
-//              int pdgID = abs(GenParticlePdgId->at(j));
-// 
-//              if( pdgID==511 || pdgID==521 || pdgID==531 || pdgID==541 || pdgID==5122 || pdgID==5132 || pdgID==5232 || pdgID==5332
-//                  || pdgID==411 || pdgID==421 || pdgID==431 || pdgID==4122 || pdgID==4132 || pdgID==4232 || pdgID==4332 )
-//              {
-//                // set GenParticle 4-vector
-//                v_gp.SetPtEtaPhiE(GenParticlePt->at(j),GenParticleEta->at(j),GenParticlePhi->at(j),GenParticleE->at(j));
-//                double deltaR = v_j.DeltaR(v_gp);
-// 
-//                if( deltaR < minDeltaR ) minDeltaR = deltaR;
-//              }
-//            }
-// 
-//            if( minDeltaR < matchingRadius )
-//            {
-//              ++nHeavyFlavorJets;
-//              isHeavyFlavor = true;
-//            }
-//          }
-//        }
-// 
-//        if( (btagger==0 && PFJetTCHE->at(v_idx_pfjet_JetID[i])>getPreCutValue1("TCHEM_WP")) ||
-//            (btagger==1 && PFJetSSVHE->at(v_idx_pfjet_JetID[i])>getPreCutValue1("SSVHEM_WP")) ||
-//            (btagger==2 && PFJetTCHP->at(v_idx_pfjet_JetID[i])>getPreCutValue1("TCHPT_WP")) ||
-//            (btagger==3 && PFJetSSVHP->at(v_idx_pfjet_JetID[i])>getPreCutValue1("SSVHPT_WP")) )
-//        {
-//          ++nBTaggedJets;
-//          if( isHeavyFlavor ) ++nBTaggedHeavyFlavorJets;
-//        }
-//      }
-//    }
+
+   if( passedAllPreviousCuts("DijetMass_bbbar") )
+   {
+     if( nBTaggedJets==0 ) FillUserTH1D("h1_DijetMass_bbbar_0tag", getVariableValue("DijetMass"), eventWeight );
+     if( nBTaggedJets==1 ) FillUserTH1D("h1_DijetMass_bbbar_1tag", getVariableValue("DijetMass"), eventWeight );
+     if( nBTaggedJets==2 ) FillUserTH1D("h1_DijetMass_bbbar_2tag", getVariableValue("DijetMass"), eventWeight );
+   }
    
    // select only those events that pass the full selection
    if( passedCut("all") ) ret = true;
